c42e0e084ac7e2b647c07c86ad81b14c4bdf6917
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2025-02-18 19:04:14 -0600

    make radio button in add creator actually work

--- girder_jsonforms/web_client/views/widgets/AddCreatorDialog.js
+++ girder_jsonforms/web_client/views/widgets/AddCreatorDialog.js
@@ -9,32 +9,35 @@ const View = girder.views.View;
 
 var AddCreatorDialog = View.extend({
   events: {
-    'submit #g-add-creator': function (event) {
+    'click .g-add-creator-btn': function (event) {
       event.preventDefault();
       this._addCreator();
-    }
+    },
+    'change input[name="creatorType"]': function (event) {
+      let creatorType = event.target.value;
+      $('#g-add-creator-form').find('.person-fields').attr('hidden', creatorType !== 'person');
+      $('#g-add-creator-form').find('.org-fields').attr('hidden', creatorType !== 'organization');
+    },
   },
 
   initialize: function (settings) {
     this.settings = settings;
     this.creators = settings.creators || [];
+    this.creatorType = settings.creatorType || 'person';
   },
 
   render: function () {
-    this.$el.html(AddCreatorDialogTemplate({})).girderModal(this);
-
-    //this.$('#creator').val(this.settings.creators[0]);
-
+    this.$el.html(AddCreatorDialogTemplate({creatorType: this.creatorType})).girderModal(this);
     return this;
   },
 
   _addCreator: function () {
-    const creator = this.$('#creator').val();
+    const creator = $('#g-add-creator-form').serializeArray();
     if (creator) {
       this.creators.push(creator);
       this.settings.parentView.render();
-      this.$el.modal('hide');
     }
+    this.$el.modal('hide');
   },
 });
 
