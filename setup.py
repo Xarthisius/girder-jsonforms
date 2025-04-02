2e128b33cea0e59d90786841b566e255b29883fa
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2025-04-02 13:13:10 -0500

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="girder-jsonforms",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="2.0.7",
+    version="2.1.0",
     description="Girder plugin adding forms based on JSON-editor",
     packages=find_packages(),
     include_package_data=True,
