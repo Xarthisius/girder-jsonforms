6286af59c3ce55abaa7f63da43bd1a2abc2a0c78
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2026-04-10 09:18:36 -0500

    Add new partition endpoints

--- girder_jsonforms/rest/aimdl.py
+++ girder_jsonforms/rest/aimdl.py
@@ -1,3 +1,6 @@
+import hashlib
+
+import dateutil.parser
 from girder.api import access
 from girder.api.describe import Description, autoDescribeRoute
 from girder.api.rest import Resource, filtermodel
@@ -6,6 +9,8 @@ from girder.exceptions import RestException
 from girder.models.collection import Collection
 from girder.models.item import Item
 
+_AIMDL_COLLECTION_ID = "665de536bcc722774ce53754"  # TODO: make this configurable
+
 
 class AIMDL(Resource):
     def __init__(self):
@@ -13,6 +18,8 @@ class AIMDL(Resource):
         self.resourceName = "aimdl"
         self.route("GET", ("datatype",), self.get_datatypes)
         self.route("GET", ("datafiles",), self.get_items_by_datatype)
+        self.route("GET", ("partition",), self.list_partitions)
+        self.route("GET", ("partition", "details"), self.get_partition)
 
     @access.user
     @autoDescribeRoute(
@@ -25,6 +32,106 @@ class AIMDL(Resource):
         Get a list of available data types for AIMDL.
         """
         return Item().collection.distinct("meta.data_type")
+
+    @access.user
+    @autoDescribeRoute(
+        Description("Get a list of IGSNS for a given dataType")
+        .param("dataType", "The data type to filter items by.", required=True)
+        .param(
+            "since",
+            "Only return items updated since this date (ISO 8601 format).",
+            required=False,
+        )
+        .errorResponse("You are not authorized to access this resource.", 403)
+    )
+    def list_partitions(self, dataType, since):
+        """
+        Get a list of IGSNS and correspoding items
+        """
+        user = self.getCurrentUser()
+        aimdl_collection = Collection().load(
+            _AIMDL_COLLECTION_ID, user=user, level=AccessType.READ, exc=True
+        )
+        q = {
+            "baseParentId": aimdl_collection["_id"],
+            "baseParentType": "collection",
+            "meta.data_type": dataType,
+            "meta.igsn": {"$exists": True},
+        }
+        if since:
+            q["updated"] = {"$gt": dateutil.parser.parse(since)}
+
+        if dataType == "xrd_raw":
+            return self._xrd_map(q, user=user)
+        else:
+            raise RestException(
+                f"Data type {dataType} is not supported for partitions."
+            )
+
+    @access.user
+    @autoDescribeRoute(
+        Description("Get a Dagster partition for a given dataType")
+        .param("key", "The partition key", required=True)
+        .param("dataType", "The data type to filter items by.", required=False)
+        .errorResponse("You are not authorized to access this resource.", 403)
+    )
+    @filtermodel(model=Item)
+    def get_partition(self, key, dataType):
+        igsn, experiment_date = key.split("//")
+        print(igsn, experiment_date)
+        user = self.getCurrentUser()
+        aimdl_collection = Collection().load(
+            _AIMDL_COLLECTION_ID, user=user, level=AccessType.READ, exc=True
+        )
+        q = {
+            "baseParentId": aimdl_collection["_id"],
+            "baseParentType": "collection",
+            "meta.igsn": igsn,
+            "meta.experiment_date": experiment_date,
+        }
+        if dataType:
+            q["meta.data_type"] = dataType
+
+        return Item().findWithPermissions(q, user=user, level=AccessType.READ)
+
+    @staticmethod
+    def _xrd_map(q, user=None):
+        fields = {
+            "meta.igsn": 1,
+            "meta.checksum": 1,
+            "meta.experiment_date": 1,
+            "folderId": 1,
+            "name": 1,
+            "_id": 1,
+        }
+
+        igsn_map = {}
+        for item in Item().findWithPermissions(
+            q, user=user, level=AccessType.READ, fields=fields
+        ):
+            # group by 'igsn//experiment_date'
+            try:
+                key = item["meta"]["igsn"] + "//" + item["meta"]["experiment_date"]
+            except KeyError:
+                print(
+                    "Item {} is missing either an IGSN or an experiment date.".format(
+                        item["_id"]
+                    )
+                )
+                continue
+            if key not in igsn_map:
+                igsn_map[key] = []
+
+            try:
+                igsn_map[key].append(item["meta"]["checksum"]["sha256"])
+            except KeyError:
+                print("Item {} is missing a sha256 checksum.".format(item["_id"]))
+        result = {}
+        for key, scans in igsn_map.items():
+            sha256 = hashlib.sha256()
+            sha256.update("".join(sorted(scans)).encode("utf-8"))
+            result[key] = sha256.hexdigest()
+        return result
 
     @access.user
     @filtermodel(model=Item)
@@ -46,9 +153,8 @@ class AIMDL(Resource):
             raise RestException("Limit cannot exceed 100.")
 
         user = self.getCurrentUser()
-        aimdl_collection_id = "665de536bcc722774ce53754"  # TODO: make this configurable
         aimdl_collection = Collection().load(
-            aimdl_collection_id, user=user, level=AccessType.READ, exc=True
+            _AIMDL_COLLECTION_ID, user=user, level=AccessType.READ, exc=True
         )
         q = {
             "meta.igsn": {"$exists": True},
